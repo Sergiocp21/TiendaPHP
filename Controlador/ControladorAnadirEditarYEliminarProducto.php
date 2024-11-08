fdde931d3a52f154490a4b968a8867227db4861a
Sergiocp <sergiocp012003@gmail.com>
2024-11-08 10:28:39 +0100

    Controlador para editar producto añadido, modificado el DAO para arreglar el nombre de la tabla en las consultas añadida vista pra editar producto

---
+++ Controlador/ControladorAnadirEditarYEliminarProducto.php
@@ -0,0 +1,58 @@
+<?php
+require_once './ControladorProducto.php';
+session_start();
+$controladorProducto = new ControladorProducto();
+$accion = $_REQUEST['gestionProductos'];
+
+
+function comprobar($campo)
+{
+
+    switch ($campo) {
+        case "nombre":
+            if ($_REQUEST['nombre'] == "") {
+                return false;
+            } else {
+                return true;
+            }
+        case "descripcion":
+            if ($_REQUEST['descripcion'] == "") {
+                return false;
+            } else {
+                return true;
+            }
+
+        case "precio":
+            if ($_REQUEST['precio'] <= 0) {
+                return false;
+            } else {
+                return true;
+            }
+    }
+}
+switch ($accion) {
+    case "Añadir Producto":
+
+        break;
+
+    case "Editar Producto":
+        if (isset($_REQUEST['idProduct']) && isset($_REQUEST['nombre']) && isset($_REQUEST['descripcion']) && isset($_REQUEST['precio'])) {
+            if (comprobar("nombre")) {
+                if (comprobar("descripcion")) {
+                    if (comprobar("precio")) {
+                        $controladorProducto->updateProduct($_REQUEST['idProduct'], $_REQUEST['nombre'], $_REQUEST['descripcion'], $_REQUEST['precio'], null);
+                        header("Location: ../Vista/VistaEditarProducto.php?idProduct=" . $_REQUEST['idProduct'] . "&exito=Producto editado correctamente");
+                    } else {
+                        header("Location: ../Vista/VistaEditarProducto.php?idProduct=" . $_REQUEST['idProduct'] . "&error=El precio debe ser un valor positivo");
+                    }
+                } else {
+                    header("Location: ../Vista/VistaEditarProducto.php?idProd=" . $_REQUEST['idProduct'] . "&error=Rellena la descripción");
+                }
+            } else {
+                header("Location: ../Vista/VistaEditarProducto.php?idProduct=" . $_REQUEST['idProduct'] . "&error=EL nombre no puede estar vacío");
+            }
+        } else {
+            header("Location: ../Vista/VistaEditarProducto.php?idProduct=" . $_REQUEST['idProduct'] . "&error=Error al editar el producto");
+        }
+        break;
+}

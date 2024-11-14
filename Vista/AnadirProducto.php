968f2a928c7d3349ca2de82f795d7e26e58fc2c3
oskiitarlaraa <oscar.lara@educa.madrid.org>
2024-11-14 09:29:46 +0100

    cambio de menu (submenu incompleto)

--- Vista/AnadirProducto.php
+++ Vista/AnadirProducto.php
@@ -20,13 +20,13 @@
 <nav class="navbar">
     <ul>
         <li><a href="index.php">Inicio</a></li>
-        <li><a href="AnadirProducto.php">Añadir Producto</a></li>
-        <li><a href="vistaEditarProducto.php">Modificar Producto</a></li>
-        <li><a href="FormularioEliminar.php">Eliminar Producto</a></li>
         <li><a href="vistaCarrito.php">Carrito</a></li>
-        <li><a href="InfoCliente.php">Cliente</a> </li>
-    </ul>
-    </li>
+        <li><a href="InfoCliente.php">Cliente</a></li>
+        <li><a href="vistaProductos.php">Productos</a>
+            <ul class="submenu">
+                <li><a href="AnadirProducto.php">Añadir Producto</a> </li>
+            </ul>
+        </li>
     </ul>
 </nav>
 
